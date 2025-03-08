28eb5962e452f4d1b444b4844223d6e75bcbe75e
Deepak Kumar Bommerla <deepakbommerla@Deepak-MacBook-Pro.local>
2025-03-07 20:41:08 -0500

    full commit

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,13 @@
+variable "ami_value" {
+    description = "enter ami value"
+  
+}
+
+variable "subnet_id_value" {
+    description = "enter subnet id value"
+  
+}
+
+variable "instance_type_value" {
+    description = "enter instance type value"
+}
